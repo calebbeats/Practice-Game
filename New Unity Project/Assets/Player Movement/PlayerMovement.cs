01c7396142cfa94985cce68f4bf2abd66f0cc382
calebbeats <calebmills1@gmail.com>
2016-12-17 14:47:51 -0600

    Added files

---
+++ New Unity Project/Assets/Player Movement/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetButton deals with the input manager. We should use this/
+		.
+		if (Input.GetButton ("fire1")) {
+			Debug.Log ("Pressed Fire1 key");
+		}
+		if (Input.GetKeyDown ("s")) {
+			Debug.Log ("Pressed S key");
+		}
+		if (Input.GetKeyUp ("a")) {
+			Debug.Log ("Pressed A key");
+		}
+		if (Input.GetKey ("d")) {
+			Debug.Log ("Pressed D key");
+		}
+		// Left mouse is 0, Right mouse is 1.
+		if (Input.GetMouseButton (0)) {
+			Debug.Log ("Left Mouse");
+		}
+	}
+}

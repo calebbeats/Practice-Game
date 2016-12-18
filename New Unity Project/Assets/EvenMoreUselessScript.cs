65119103ed6ebcabe7cca6b8c7da0f4327d0c23f
Josh <Schythed@gmail.com>
2016-12-18 15:19:12 -0600

    Delete file and Added file
    asghouigfh

--- New Unity Project/Assets/EvenMoreUselessScript.cs
+++ New Unity Project/Assets/EvenMoreUselessScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DELETETHIS : MonoBehaviour {
+public class EvenMoreUselessScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
